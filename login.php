9334fedf78ec838a5152c105d992065b7587713a
MukwevhoDN <mukwevhoneville59@gmail.com>
2024-02-16 20:38:55 +0200

    Login page
    -Add required on the login page to make sure users do not press login without filling the username and password
    -Change function that when login details = false then it must stay at the same page

--- login.php
+++ login.php
@@ -3,16 +3,16 @@ session_start();
 
 // Establish connection to MySQL database
 $servername = "localhost:3306";
-$username = "root"; // Update with your MySQL username
-$password = "Neville@2001"; // Update with your MySQL password
-$database = "bisi"; // Update with your MySQL database name
+$username = "root"; // MySQL username
+$password = "Neville@2001"; // MySQL password
+$database = "bisi"; // MySQL database name
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $database);
 
 // Check connection
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    die("Connection failed TRY AGAIN LATER: " . $conn->connect_error);
 }
 
 // Check if the form has been submitted
@@ -40,13 +40,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if ($result->num_rows > 0) {
         // Store user information in session
         $user = $result->fetch_assoc();
-        $_SESSION['username'] = $user['Username']; // Assuming 'Username' is the unique identifier
+        $_SESSION['username'] = $user['Username']; 
         // Redirect to a logged-in page
         header("Location: main.html");
         exit(); // Terminate script after redirection
     } else {
         // If no row was returned, display error message or redirect back to login page
-        echo "Invalid username or password";
+        //echo '<script>alert("Incorrect login credentials");</script>';
+         header("Location: Welcome.html");
+        //echo "Invalid username or password";
+      
     }
 
     // Close statement
